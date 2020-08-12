c80062c06f75fa712fe16107e41521ded1a9233a
multitalented <39840211+multitalented@users.noreply.github.com>
2020-08-12 13:12:29 -0400

    Updating with files

---
+++ app.js
@@ -0,0 +1,14 @@
+document.getElementById("heading").innerHTML = 
+    localStorage["title"] || "Just Write"; // default text
+document.getElementById("content").innerHTML = 
+    localStorage["text"] || "This text is automatically saved every second :)"
+        // default text for content
+
+setInterval(function() {
+    // function that is saving the innerHTML of the div
+    localStorage["title"] = document.getElementById("heading").innerHTML;
+        // heading div
+    localStorage["text"] = document.getElementById("content").innerHTML;
+        // content div
+}, 1000);
+
